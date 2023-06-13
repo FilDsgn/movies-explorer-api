e7600590915328e9078fd280a31683a3d0a04dda
Alexander Filimonov <fil.dsgn@yandex.ru>
2023-06-14 02:49:05 +0300

    dev: add user controllers

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,31 @@
 const User = require('../models/user');
 
+const NotFoundError = require('../errors/NotFoundError');
+const BadRequestError = require('../errors/BadRequestError');
+
 const getUsers = (req, res, next) => {
   User.find({})
     .then((users) => res.send(users))
     .catch(next);
 };
 
-module.exports = { getUsers };
+const updateUsers = (req, res, next) => {
+  const { name, email } = req.body;
+
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, email },
+    { new: true, runValidators: true },
+  )
+    .orFail(new NotFoundError('Пользователь с указанным _id не найден.'))
+    .then((user) => res.send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return next(new BadRequestError('Переданы некорректные данные при обновлении профиля.'));
+      }
+
+      return next(err);
+    });
+};
+
+module.exports = { getUsers, updateUsers };
