929873591cfc9fbbc9c53516b5c5cc88fb9a7ed7
Alexander Filimonov <fil.dsgn@yandex.ru>
2023-06-16 00:02:40 +0300

    dev: add auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,31 @@
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+const jwt = require('jsonwebtoken');
+
+const NotAuthError = require('../errors/NotAuthError');
+
+// eslint-disable-next-line consistent-return
+module.exports = (req, res, next) => {
+  const { authorization } = req.body;
+
+  if (!authorization || authorization.startsWith('Bearer ')) {
+    return next(new NotAuthError('Необходима авторизация'));
+  }
+
+  const token = authorization.replace('Bearer ', '');
+
+  let payload;
+
+  try {
+    payload = jwt.verify(
+      token,
+      NODE_ENV === 'production' ? JWT_SECRET : 'jwt-secret',
+    );
+  } catch (err) {
+    return next(new NotAuthError('Необходима авторизация'));
+  }
+
+  req.user = payload;
+
+  next();
+};
