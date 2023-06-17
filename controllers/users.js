d056d90a2623ab822d6cf865f7558691cc2d2d9a
Alexander Filimonov <fil.dsgn@yandex.ru>
2023-06-17 19:50:22 +0300

    dev: add login and createUser controllers

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,12 @@
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 const NotFoundError = require('../errors/NotFoundError');
 const BadRequestError = require('../errors/BadRequestError');
+const ConflictError = require('../errors/ConflictError');
 
 const getUsers = (req, res, next) => {
   User.find({})
@@ -37,4 +42,46 @@ const updateUser = (req, res, next) => {
     });
 };
 
-module.exports = { getUsers, getUserInfo, updateUser };
+const createUser = (req, res, next) => {
+  const { email, password } = req.body;
+
+  bcrypt.hash(password, 10)
+    .then((hash) => {
+      User.create({
+        email,
+        password: hash,
+      })
+        .then(() => res.status(201).send({ email }))
+        .catch((err) => {
+          if (err.name === 'ValidationError') {
+            return next(new BadRequestError('Переданы некорректные данные при создании пользователя.'));
+          }
+          if (err.code === 11000) {
+            return next(new ConflictError('Такой пользователь уже существует'));
+          }
+
+          return next(err);
+        });
+    })
+    .catch(next);
+};
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'jwt-secret', { expiresIn: '7d' });
+
+      res.send(token);
+    })
+    .catch(next);
+};
+
+module.exports = {
+  getUsers,
+  getUserInfo,
+  updateUser,
+  createUser,
+  login,
+};
--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcryptjs');
+
+const NotAuthError = require('../errors/NotAuthError');
 
 const userSchema = mongoose.Schema({
   email: {
@@ -24,5 +27,23 @@ const userSchema = mongoose.Schema({
     required: true,
   },
 });
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new NotAuthError('Неправильные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new NotAuthError('Неправильные почта или пароль'));
+          }
+
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
